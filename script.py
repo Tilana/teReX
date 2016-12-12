cc660bda84c0145f47655fe736f62ed3bd212ccd
tilana <NatalieWidmann@gmx.de>
2016-12-12 11:13:27 +0100

    semi-supervised Learning added

--- script.py
+++ script.py
@@ -3,8 +3,12 @@ from py3neo import Graph, Node, Relationship
 from sklearn.datasets import fetch_20newsgroups
 import pandas as pd 
 import numpy as np
+from numpy import transpose, identity
 from preprocessing import standardPreprocessing 
 import os.path
+from sklearn.semi_supervised import LabelPropagation
+from sklearn.metrics import accuracy_score
+from scipy import sparse
 
 def script():
     
@@ -25,13 +29,13 @@ def script():
         
     data = pd.read_pickle(filename)
 
-    toydata = [[0, [['This','is','it','.'],['it','.']]], [1,[['it','is','here','is','.']]]]
-    data = pd.DataFrame(toydata, columns=['category', 'sentences'])
+    #toydata = [[0, [['This','is','it','.'],['it','.']]], [1,[['it','is','here','is','.']]]]
+    #data = pd.DataFrame(toydata, columns=['category', 'sentences'])
 
     print 'Graph Construction'
     wordID = 1
     startNode = database.createFeatureNode(0,'$Start$')
-    for index, text in enumerate(data.sentences[0:2]):
+    for index, text in enumerate(data.sentences):
         print 'Document' + str(index)
         label = data.category.loc[index]
         docNode = database.createDocumentNode(index, label)
@@ -61,6 +65,33 @@ def script():
     docFeatureMatrix = database.getMatrix(featureNodes, docNodes, 'is_in')
     combinedMatrix = np.concatenate((np.transpose(docFeatureMatrix),featureMatrix))
     print combinedMatrix
+    np.save('NormMatrix600samples', combinedMatrix)
+
+    combinedMatrix = np.load('NormMatrix600samples.npy')    
+    docNr = 300;
+    docdoc = identity(docNr)
+    docFeatures = combinedMatrix[0:docNr,:]
+    featureDoc = transpose(docFeatures)
+    featureFeature = combinedMatrix[601:601+docNr,:]
+
+    docAll = np.concatenate((docdoc, docFeatures), axis=1)
+    featureAll = np.concatenate((featureDoc, featureFeature), axis=1)
+    
+
+    X = np.concatenate((docAll,featureAll)) 
+    nrLabeledData = 100
+
+    #sparseMatrix = sparse.csr_matrix(combinedMatrix)
+    print 'Label Propagation'
+    labelPropagation = LabelPropagation(alpha=1) 
+    labels = np.ones([X.shape[0]])*-1
+    trueLabelIndex = range(0,nrLabeledData)
+    labels[trueLabelIndex] = data.loc[trueLabelIndex, 'category'].tolist()
+
+    labelPropagation.fit(X, labels)
+    predictLabels = labelPropagation.transduction_
+    print predictLabels
+    print accuracy_score(data.category[0:600].tolist(), predictLabels.tolist())
 
 
 if __name__ == '__main__':
