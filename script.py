f991e5bb6934bde9455ba66cce95155ae40b2259
tilana <nataliewidmann@gmx.de>
2017-01-27 12:52:01 +0100

    minor changes

--- script.py
+++ script.py
@@ -9,12 +9,11 @@ from numpy import transpose, identity
 from preprocessing import standardPreprocessing, lemmatizeAll, tokenizeSentence
 import os.path
 
-
 def script():
     
     database  = GraphDatabase()
     filename = 'processedDocuments/Newsgroup_guns_motorcycles_10.pkl'
-    minFrequency = 5
+    minFrequency = 2
 
     if not os.path.exists(filename):
         print 'Load Documents'
@@ -22,7 +21,7 @@ def script():
 	data = fetch_20newsgroups(categories=['talk.politics.guns', 'rec.motorcycles'], remove=('headers', 'footers', 'quotes'))
 	categories = data.target_names
 	data = pd.DataFrame({'text': data['data'], 'category': data['target']})
-	#data = data[9:11]
+	data = data[0:10]
 
         for index, category in enumerate(categories):
             print 'Category: ' + category + '   N: ' + str(len(data[data.category==index]))
@@ -36,7 +35,6 @@ def script():
         print('Number of Unique words: %d' % len(vocabulary))
         print('Minimal Frequency: %d' % minFrequency)
 
-	
 	#tfIdf = TfidfVectorizer(min_df = minFrequency, stop_words='english', token_pattern='[a-zA-Z]+')
 	#tfIdfwords = tfIdf.fit_transform(docs)
 	#vocabulary = tfIdf.get_feature_names()
@@ -75,53 +73,51 @@ def script():
 	data.index = range(len(data))
 		
         data.to_pickle(filename)
-        
-    #data = pd.read_pickle(filename)
-    #data = data[0:5]
+	
+	#data = pd.read_pickle(filename)
+	#data = data[0:5]
 
-    #toydata = [[0, [['This','is','it','.'],['it','.']]], [1,[['it','is','here','is','.']]]]
-    #data = pd.DataFrame(toydata, columns=['category', 'sentences'])
+	#toydata = [[0, [['This','is','it','.'],['it','.']]], [1,[['it','is','here','is','.']]]]
+	#data = pd.DataFrame(toydata, columns=['category', 'sentences'])
 
-    print 'Graph Construction'
-    wordID = -1
-    #wordID = 1
-    startNode = database.createFeatureNode(wordID,'$Start$')
-    for index, text in enumerate(data.sentences):
-        print 'Document' + str(index)
-        label = data.category.loc[index]
-        docNode = database.createDocumentNode(index, label)
-        for sentence in text:
-            preceedingWord = startNode 
-            for word in sentence:
-		exists = len(list(database.graph.find('Feature', property_key='word', property_value=word))) > 0
-		if not exists:
-		    wordID = vocabulary[word]
-                    wordNode = database.createFeatureNode(wordID, word)
-                    #wordID += 1 
-                else:
-                    wordNode = database.getFeatureNode(word)
-                database.createWeightedRelation(wordNode, docNode, 'is_in')
-                database.createWeightedRelation(preceedingWord, wordNode, 'followed_by')
-                preceedingWord = wordNode
+	print 'Graph Construction'
+	wordID = -1
+	#wordID = 1
+	startNode = database.createFeatureNode(wordID,'$Start$')
+	for index, text in enumerate(data.sentences):
+		print 'Document' + str(index)
+		label = data.category.loc[index]
+		docNode = database.createDocumentNode(index, label)
+		for sentence in text:
+			preceedingWord = startNode
+			for word in sentence:
+				exists = len(list(database.graph.find('Feature', property_key='word', property_value=word))) > 0
+				if not exists:
+					wordID = vocabulary[word]
+					wordNode = database.createFeatureNode(wordID, word)
+					wordID += 1
+				else:
+					wordNode = database.getFeatureNode(word)
+					database.createWeightedRelation(wordNode, docNode, 'is_in')
+					database.createWeightedRelation(preceedingWord, wordNode, 'followed_by')
+					preceedingWord = wordNode
 
+	print 'Normalize relationships'
+	docNodes = database.getNodes('Document')
+	database.normalizeRelationships(docNodes, 'is_in')
+	
+	featureNodes = database.getNodes('Feature')
+	database.normalizeRelationships(featureNodes, 'followed_by')
 
-    print 'Normalize relationships'
-    docNodes = database.getNodes('Document')
-    database.normalizeRelationships(docNodes, 'is_in')
-
-    featureNodes = database.getNodes('Feature')
-    database.normalizeRelationships(featureNodes, 'followed_by')
-
-    print 'Create Matrix'
-    docMatrix = identity(len(docNodes))
-    featureMatrix = database.getMatrix(featureNodes)
-    featureDocMatrix = database.getMatrix(featureNodes, docNodes, 'is_in')
-    docAll = np.concatenate((docMatrix, np.transpose(featureDocMatrix)), axis=1)
-    featureAll = np.concatenate((featureDocMatrix, featureMatrix), axis=1)
-    combinedMatrix = np.concatenate((docAll, featureAll))
-    print combinedMatrix.shape
-    np.save('guns_motorcycles_10', combinedMatrix)
-
+	print 'Create Matrix'
+	docMatrix = identity(len(docNodes))
+	featureMatrix = database.getMatrix(featureNodes)
+	featureDocMatrix = database.getMatrix(featureNodes, docNodes, 'is_in')
+	docAll = np.concatenate((docMatrix, np.transpose(featureDocMatrix)), axis=1)
+	featureAll = np.concatenate((featureDocMatrix, featureMatrix), axis=1)
+	combinedMatrix = np.concatenate((docAll, featureAll))
+	print combinedMatrix.shape
+	np.save('guns_motorcycles_10', combinedMatrix)
 
 
 if __name__ == '__main__':
